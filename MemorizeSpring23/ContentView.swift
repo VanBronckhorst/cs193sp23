45e3899daece031e4af8780c206ff565bb25628e
Filippo Pellolio <filippo.pellolio@mail.polimi.it>
2023-10-30 19:06:54 -0700

    Initial Commit

---
+++ MemorizeSpring23/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MemorizeSpring23
+//
+//  Created by Filippo Pellolio on 10/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MemorizeSpring23/MemorizeSpring23App.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeSpring23App.swift
+//  MemorizeSpring23
+//
+//  Created by Filippo Pellolio on 10/30/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeSpring23App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
