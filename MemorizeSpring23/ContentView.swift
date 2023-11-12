cd890ecdebc293eac29b36f738c684c5ae2e0e91
Filippo Pellolio <filippo.pellolio@mail.polimi.it>
2023-11-11 22:21:23 -0800

    Initial Impl

--- MemorizeSpring23/ContentView.swift
+++ MemorizeSpring23/ContentView.swift
@@ -11,21 +11,23 @@ struct Theme {
   let name : String
   let emojis : [String]
   let symbol : Image
+  let cardColor : Color
 }
 
 let animalEmojis = ["🦁", "🐸", "🦊", "🐼", "🐢", "🦉", "🦒", "🐳", "🦔", "🐍", "🦖", "🦩", "🐘", "🦝", "🐪"]
-let animalTheme = Theme(name: "Animals", emojis: animalEmojis, symbol: Image(systemName: "pawprint"))
+let animalTheme = Theme(name: "Animals", emojis: animalEmojis, symbol: Image(systemName: "pawprint"), cardColor: .green)
 
 let travelEmojis = ["✈️", "🌍", "🗼", "🏝", "🚂", "🌅", "🗽", "🏜", "🚠", "🛶", "🗺", "⛩", "🌉", "🚗"]
-let travelTheme = Theme(name: "Travel", emojis: travelEmojis, symbol: Image(systemName: "car"))
+let travelTheme = Theme(name: "Travel", emojis: travelEmojis, symbol: Image(systemName: "car"), cardColor: .blue)
 
-let foodEmojis = ["🍉", "🍕", "🍣", "🍦", "🥥", "🍍", "🍔", "🍫", "🥐", "🍓", "🍱", "🍺", "🍪"]
-let foodTheme = Theme(name: "Food", emojis: foodEmojis, symbol: Image(systemName: "fork.knife"))
+let foodEmojis = ["🍉", "🍕", "🍣", "s🍦", "🥥", "🍍", "🍔", "🍫", "🥐", "🍓", "🍱", "🍺", "🍪"]
+let foodTheme = Theme(name: "Food", emojis: foodEmojis, symbol: Image(systemName: "fork.knife"), cardColor: .brown)
 
 struct ContentView: View {
     let availableThemes = [animalTheme, foodTheme, travelTheme]
 
     @State var selectedEmojis = animalEmojis.flatMap{[$0, $0]}.shuffled()
+    @State var cardColor = animalTheme.cardColor
 
     var body: some View {
       VStack {
@@ -38,29 +40,47 @@ struct ContentView: View {
       }.padding()
     }
 
-  var cards : some View {
-    LazyVGrid(columns: [GridItem(.adaptive(minimum: 50))]) {
-      ForEach(0..<selectedEmojis.count, id: \.self) { index in
-        CardView(icon: selectedEmojis[index]).aspectRatio(2/3 , contentMode: .fit)
-      }
-    }.foregroundColor(.blue)
-  }
+    var cards : some View {
+      LazyVGrid(columns:
+                  [GridItem(.adaptive(minimum: widthThatBestFits(selectedEmojis.count)))]) {
+        ForEach(0..<selectedEmojis.count, id: \.self) { index in
+          CardView(icon: selectedEmojis[index]).aspectRatio(2/3 , contentMode: .fit)
+        }
+      }.foregroundColor(cardColor)
+    }
 
-  var themeButtons : some View {
-    HStack{
-      ForEach(availableThemes, id:\.name) { theme in
-        Button(action: {
-          selectedEmojis = theme.emojis.flatMap{[$0, $0]}.shuffled()
-        }) {
-          VStack {
-            theme.symbol
-            Text(theme.name).font(.caption)
+    var themeButtons : some View {
+      HStack (alignment: .bottom, spacing: 40){
+        ForEach(availableThemes, id:\.name) { theme in
+          Button(action: {
+            selectedEmojis =
+              theme
+                .emojis[0...Int.random(in: 1..<theme.emojis.count)]
+                .flatMap{[$0, $0]}.shuffled()
+            cardColor = theme.cardColor
+          }) {
+            VStack(alignment: .center) {
+              theme.symbol
+              Text(theme.name).font(.caption)
+            }
           }
         }
       }
     }
-  }
 
+  func widthThatBestFits(_ cardsCount: Int) -> CGFloat {
+    let minWidth : CGFloat = 50
+
+    if (cardsCount < 5) {
+      return minWidth * CGFloat(3)
+    }
+
+    if (cardsCount < 9) {
+      return minWidth * CGFloat(2)
+    }
+
+    return minWidth
+  }
 }
 
 struct CardView: View {
